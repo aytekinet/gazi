b010a66a58bab07a40635266046c7ea76034c3f9
Aytekin <76602789+aytekinet@users.noreply.github.com>
2022-05-14 17:58:36 +0300

    def ile hesap makinesi
    def ve return kullanımı

---
+++ ders dosyalari/hesapmakinesi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 14 17:29:54 2022
+
+@author: Gazi TF
+"""
+
+def topla(a,b):
+    return a+b
+def çıkar(a,b):
+    return a-b
+def böl(a,b):
+    return a/b
+def çarp(a,b):
+    return a*b
+
